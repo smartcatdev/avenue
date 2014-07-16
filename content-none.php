b4949e3cd6f2e9b3ed82e66bdeb78f5076a3a6ce
Patrick Zapanta <pzapanta@outlook.com>
2014-07-16 10:09:33 -0400

    Fix Search Results Page

--- content-none.php
+++ content-none.php
@@ -8,26 +8,31 @@
  */
 ?>
 
-<section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'avenue' ); ?></h1>
-	</header><!-- .page-header -->
+<div class="page-title">
+	<h1 class="row center"><?php _e( 'Nothing Found', 'avenue' ); ?></h1>
+</div><!-- .page-title -->
 
-	<div class="page-content">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'avenue' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-		<?php elseif ( is_search() ) : ?>
-
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'avenue' ); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php else : ?>
-
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'avenue' ); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php endif; ?>
+<div class="row">
+	<div class="page-content col-md-12">
+		<div class="col-md-9">
+			<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+		
+				<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'avenue' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+		
+			<?php elseif ( is_search() ) : ?>
+		
+				<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'avenue' ); ?></p>
+				<?php get_search_form(); ?>
+		
+			<?php else : ?>
+		
+				<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'avenue' ); ?></p>
+				<?php get_search_form(); ?>
+		
+			<?php endif; ?>
+		</div>
+		<div class="col-md-3">
+        	<?php get_sidebar(); ?>
+		</div>
 	</div><!-- .page-content -->
-</section><!-- .no-results -->
+</div><!-- .row -->
--- search.php
+++ search.php
@@ -7,39 +7,41 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div class="site-content">
+		<div class="col-md-12">
+			<?php if ( have_posts() ) : ?>
+				<div class="page-title">
+					<h1 class="row center"><?php printf( __( 'Search Results for: %s', 'avenue' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				</div><!-- .page-title -->
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'avenue' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'content', 'search' );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php avenue_paging_nav(); ?>
-
+				<?php /* Start the Loop */ ?>
+				<div class="row">
+					<div class="page-content col-md-12">
+						<div class="col-md-9">
+							<?php while ( have_posts() ) : the_post(); ?>
+				
+								<?php
+								/**
+								 * Run the loop for the search to output the results.
+								 * If you want to overload this in a child theme then include a file
+								 * called content-search.php and that will be used instead.
+								 */
+								get_template_part( 'content', 'search' );
+							?>
+							<?php endwhile; ?>
+	
+							<?php avenue_paging_nav(); ?>
+						</div>
+						<div class="col-md-3">
+                        	<?php get_sidebar(); ?>
+						</div>
+					</div>
+				</div>
 		<?php else : ?>
-
 			<?php get_template_part( 'content', 'none' ); ?>
-
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
+		</div><!-- .col-md-12 -->
+	</div><!-- .site-content -->
+	
 <?php get_footer(); ?>
